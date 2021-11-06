aed78b3568e036562074b5c165ccb25ab30f7355
connykrautstingl <s44784@edu.campus02.at>
2021-11-06 13:32:59 +0100

    added stack

--- src/org/campus02/stack/DemoStackApp.java
+++ src/org/campus02/stack/DemoStackApp.java
@@ -1,4 +1,22 @@
 package org.campus02.stack;
 
 public class DemoStackApp {
+
+    public static void main(String[] args) {
+
+
+        Stack stapel = new Stack(new int[7]);
+
+        stapel.push(7);
+        stapel.push(5);
+        stapel.push(6);
+
+        System.out.println("stapel.pop() = " + stapel.pop());
+        System.out.println("stapel.pop() = " + stapel.pop());
+        stapel.push(3);
+        System.out.println("stapel.pop() = " + stapel.pop());
+        System.out.println("stapel.pop() = " + stapel.pop());
+    }
+
+
 }
